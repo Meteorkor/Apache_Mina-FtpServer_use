2eead84f461f5241e772b394576e1495266049e6
meteorkor <meteor_kor@gmail.com>
2013-12-06 13:14:25 +0900

    se

--- Apache_Mina_FtpServer_use/src/com/meteor/main/Main.java
+++ Apache_Mina_FtpServer_use/src/com/meteor/main/Main.java
@@ -1,9 +1,12 @@
 package com.meteor.main;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
 import org.apache.ftpserver.ftplet.FtpException;
 
 import org.apache.ftpserver.ftplet.UserManager;
@@ -11,9 +14,11 @@ import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.ssl.SslConfigurationFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 import org.apache.ftpserver.usermanager.SaltedPasswordEncryptor;
-import org.apache.ftpserver.usermanager.UserFactory;
+
 import org.apache.ftpserver.usermanager.impl.BaseUser;
 import org.apache.ftpserver.usermanager.impl.WritePermission;
+
+import com.meteor.module.Ftp_UserManager;
 
 public class Main {
 
@@ -26,8 +31,8 @@ public class Main {
 
 		/**/						
 		SslConfigurationFactory ssl = new SslConfigurationFactory();
-		//ssl.setKeystoreFile(new File("c://stone//stone.txt"));
-		ssl.setKeystoreFile(new File("src/test/resources/ftpserver.jks"));
+		ssl.setKeystoreFile(new File("c://stone//stone.txt"));
+		//ssl.setKeystoreFile(new File("src/test/resources/ftpserver.jks"));
 		ssl.setKeystorePassword("password");
 		
 		ListenerFactory factory = new ListenerFactory();
@@ -39,32 +44,15 @@ public class Main {
 		*/
 		
 		serverFactory.addListener("default", factory.createListener());
-		PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
-		userManagerFactory.setFile(new File("c://stone/stone.properties") );
-		userManagerFactory.setPasswordEncryptor(new SaltedPasswordEncryptor());
-		UserManager um =userManagerFactory.createUserManager();
-		
-		
-		BaseUser user = new BaseUser();
-		user.setName("kim");
-		user.setPassword("qqq");
-		user.setHomeDirectory("c://stone");
-		
-		WritePermission wri = new WritePermission();
 		
 		
 		
+		Ftp_UserManager um = new Ftp_UserManager();
+		//um.create_user("lee", "11", true);
+		um.delete_user("lee");
 		
-		
-		try {
-			um.save(user);
-		} catch (FtpException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		serverFactory.setUserManager(userManagerFactory.createUserManager());
-		
+		//serverFactory.setUserManager(userManagerFactory.createUserManager());
+		serverFactory.setUserManager(um.get_UserManager());
 		serverFactory.addListener("default", factory.createListener());
 
 		
